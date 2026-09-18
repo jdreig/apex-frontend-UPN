b56a7dcf31866a9a47c2cefdaa00de183cb6df2e
jdreig <dreig.jdsc.1991@gmail.com>
2026-09-18 05:51:07 -0500

    fix: actualizar URL de la API a Render en ConfigService 3

--- src/app/core/services/config/config.services.ts
+++ src/app/core/services/config/config.services.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ConfigService {
-    private readonly apiUrl = 'https://tu-backend-apex.onrender.com/api';
+    private readonly apiUrl = 'https://apex-backend-api-z0oy.onrender.com/api';
   //private readonly apiUrl = 'http://localhost:8088/api';
 
   getApiUrl(): string {
